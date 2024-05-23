17539f55102ddb9326c59be3c26eeb963a03259c
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2024-05-23 09:02:44 +0530

    fiex:socket url client

--- socket.ts
+++ socket.ts
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://chat-service-o3sa.onrender.com", {
     transports:["websocket", "polling"],
     
 });
