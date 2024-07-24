5e2984298df76d273a1bd5cd84d86b52573b5729
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2024-07-24 14:38:56 +0530

    fix: base url

--- app/config/axios.tsx
+++ app/config/axios.tsx
@@ -6,8 +6,8 @@ const Axios = axios.create({
     // baseURL:  process.env.NODE_ENV === 'production' 
     //           ? 'https://talk-flow-sever2.onrender.com' 
     //           : 'http://localhost:8000/api',
-    // baseURL: 'https://talk-flow-sever2.onrender.com/api',
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://talk-flow-sever2.onrender.com/api',
+    // baseURL: 'http://localhost:8000/api',
     
     headers:{
         "Content-Type": "application/json",
